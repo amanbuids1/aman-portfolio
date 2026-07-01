b5fe933efd90baf1408ac09ee49565e02b04c9b8
amanbuids1 <amanbuilds37@gmail.com>
2026-07-01 14:30:58 +0530

    Update .gitignore and add contact form

---
+++ src/component/contack.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+export default function ContactForm() {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    formData.append("access_key", "ce27e3e7-32bf-4e9f-8840-2bf0ea8c0f95");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+    setResult(data.success ? "Success!" : "Error");
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" name="name" required/>
+      <input type="email" name="email" required/>
+      <textarea name="message" required></textarea>
+      <button type="submit">Submit</button>
+      <p>{result}</p>
+    </form>
+  );
+}
